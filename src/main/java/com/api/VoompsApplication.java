2fb28c5701c9b45cb39d637c2a0f3b236ac3d447
rohit-sawant <sawant.rohit510@gmail.com>
2021-10-04 16:34:35 +0530

    first commit

--- src/main/java/com/api/VoompsApplication.java
+++ src/main/java/com/api/VoompsApplication.java
@@ -2,6 +2,8 @@ package com.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class VoompsApplication {
